4a97a6e52aff4fd45cc27b8121a2bf4c52911ca3
teutonic <asiaticstatic@hotmail.com>
2017-10-01 17:17:33 -0700

    exercise6

---
+++ friends.py
@@ -0,0 +1,19 @@
+#make friends list
+friends = []
+
+#write down their names, age and city
+
+for friend_number in range(0,5):
+    new_friend = {'name': 'portlander', 'age': 25, 'city': 'Portland'}
+    friends.append(new_friend)
+
+for friend in friends[0,3]:
+    if friend[]
+friend_1 = {'name': 'Heather', 'age': 41}
+friend_2 = {'name': 'sarah', 'age': 39
+}
+
+print(friend_1['age'])
+print(friend_1['name'])
+#for name, age in friends():
+#    print(name.title() + " 's friends is ")
